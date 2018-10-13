736e66b69d5dd5b98a6f115fbbe21754aa125e43
Tora Desenvolvimento <assinaturas.torausifast@gmail.com>
2018-10-13 11:07:35 -0300

    Include method Sum

---
+++ RestWithAspnet_Udemy 01 - Calculator/RestWithAspnet_Udemy/Controllers/CalculatorController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspnet_Udemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+       
+
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest( "Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string vNumber)
+        {
+            try
+            {
+                decimal decimalValue;
+                if (decimal.TryParse(vNumber, out decimalValue))
+                {
+                    return decimalValue;
+                }
+                return 0;
+            }
+            catch (Exception)
+            {
+
+                throw new NotImplementedException();
+            }
+        }
+
+        private bool IsNumeric(string vNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(vNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
