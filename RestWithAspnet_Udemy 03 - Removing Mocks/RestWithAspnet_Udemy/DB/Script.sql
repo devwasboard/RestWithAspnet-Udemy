e781529360eca6512a2f2f735fda1125562d9c94
Tora Desenvolvimento <assinaturas.torausifast@gmail.com>
2018-10-14 02:55:20 -0300

    The application with connection to database rest_with_asp_net_udemy, table persons.

---
+++ RestWithAspnet_Udemy 03 - Removing Mocks/RestWithAspnet_Udemy/DB/Script.sql
@@ -0,0 +1,9 @@
+use rest_with_asp_net_udemy;
+create table person( 
+						Id int(10) unsigned null default null,
+                        FirstName varchar(50) null default null,
+                        LasttName varchar(50) null default null,
+                        Adress varchar(50) null default null,
+                        Gender varchar(50) null default null
+
+)engine = InnoDB;
---
+++ RestWithAspnet_Udemy 03 - Removing Mocks/RestWithAspnet_Udemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAspnet_Udemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithAspnet_Udemy.Services.Implimentation
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delet(long id);
+    }
+}
