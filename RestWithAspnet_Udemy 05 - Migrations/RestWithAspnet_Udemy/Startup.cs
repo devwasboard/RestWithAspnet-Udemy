d3bfa96c1f3d4a1f054b719bf707fabaf289be6d
Tora Desenvolvimento <assinaturas.torausifast@gmail.com>
2018-10-16 07:51:56 -0300

    Include in project files .sql for migrations.

--- RestWithAspnet_Udemy 05 - Migrations/RestWithAspnet_Udemy/Startup.cs
+++ RestWithAspnet_Udemy 05 - Migrations/RestWithAspnet_Udemy/Startup.cs
@@ -4,34 +4,68 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 using RestWithAspnet_Udemy.Model.Context;
 using RestWithAspnet_Udemy.Business.Implimentation;
 using RestWithAspnet_Udemy.Business;
 using RestWithAspnet_Udemy.Repository;
 using RestWithAspnet_Udemy.Repository.Implimentation;
 
+
+
+using System;
+using System.Collections.Generic;
+
 namespace RestWithAspnet_Udemy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+       
 
-        public IConfiguration Configuration { get; }
+        private readonly ILogger _logger;
+        public IConfiguration _configuration { get; }
+        public IHostingEnvironment _environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment, ILogger<Startup> logger)
+        {
+            _configuration = configuration;
+            _environment = environment;
+            _logger = logger;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
-            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+            var connectionString = _configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connectionString));
+
+            if (_environment.IsDevelopment())
+            {
+                try
+                {
+                    var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+                    var evolve = new Evolve.Evolve("evolve.json", evolveConnection, msg => _logger.LogInformation(msg))
+                    {
+                        Locations = new List<string> { "db/migrations" },
+                        //Not erase data when to run migrations
+                        IsEraseDisabled = true,
+                    };
+                    evolve.Migrate();
+                }
+                catch (Exception ex)
+                {
+
+                    _logger.LogCritical("Database migration failed", ex.Message);
+                    throw;
+                }
+            }
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
 
             //Configure version Api -> https://github.com/microsoft/aspnet-api-Versioning/wiki/Versioning-via-the-URL-Path
-            services.AddApiVersioning();
+            services.AddApiVersioning(opttion => opttion.ReportApiVersions = true);
 
             //Dependency injection
             services.AddScoped<IPersonBusiness, PersonBusinessImpl>();
